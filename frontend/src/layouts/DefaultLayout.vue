db92ded47607b706b43394a86212b6f3b3da3cff
dekadii01 <dekjawa8@gmail.com>
2026-06-14 19:23:54 +0800

    feat: enhance RealtimeSection and DefaultLayout with improved structure and conditional rendering for navbar and footer

--- frontend/src/layouts/DefaultLayout.vue
+++ frontend/src/layouts/DefaultLayout.vue
@@ -1,7 +1,25 @@
 <template>
-  <TheNavbar />
+  <TheNavbar v-if="!hideNavbar" />
+
   <slot />
+
+  <TheFooter v-if="!hideFooter" />
 </template>
+
 <script setup>
+import { computed } from "vue";
+import { useRoute } from "vue-router";
+
 import TheNavbar from "../components/TheNavbar.vue";
+import TheFooter from "../components/TheFooter.vue";
+
+const route = useRoute();
+
+const hideNavbar = computed(() => {
+  return ["/login", "/register"].includes(route.path);
+});
+
+const hideFooter = computed(() => {
+  return ["/login", "/register"].includes(route.path);
+});
 </script>
