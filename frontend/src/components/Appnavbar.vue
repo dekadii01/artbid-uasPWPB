7b7a9454b7d79ce58936e44343196166af4404ef
dekadii01 <dekjawa8@gmail.com>
2026-06-17 19:34:54 +0800

    feat: remove "Riwayat Lelang" link from profile dropdown menu

--- frontend/src/components/Appnavbar.vue
+++ frontend/src/components/Appnavbar.vue
@@ -47,16 +47,6 @@ const profileMenuItems = [
     },
   },
   {
-    label: "Riwayat Lelang",
-    to: "/history",
-    icon: {
-      render: () =>
-        iconPath(
-          "M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2",
-        ),
-    },
-  },
-  {
     label: "Pengaturan",
     to: "/settings",
     icon: {
