67d62b415cde8cf7bbbaacec387e353f94517743
dekadii01 <dekjawa8@gmail.com>
2026-06-19 00:22:17 +0800

    feat: add watchlist index page with status filtering and auction tracking stats

--- frontend/src/views/watchlist/Index.vue
+++ frontend/src/views/watchlist/Index.vue
@@ -196,6 +196,27 @@ function statusBadge(status) {
   };
   return map[status] ?? map.ended;
 }
+
+function activityStyle(type) {
+  const map = {
+    price: {
+      bg: "bg-black",
+      icon: "text-white",
+      path: "M13 7h8m0 0V3m0 4l-8 8-4-4-6 6",
+    },
+    bids: {
+      bg: "bg-gray-100",
+      icon: "text-gray-700",
+      path: "M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0z",
+    },
+    upcoming: {
+      bg: "bg-gray-100",
+      icon: "text-gray-500",
+      path: "M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z",
+    },
+  };
+  return map[type] ?? map.bids;
+}
 </script>
 
 <template>
