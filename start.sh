f20f7316a013e56e0b83424fe28e8ce5a9d1b2d9
Adi Pramana <109329655+dekadii01@users.noreply.github.com>
2026-06-21 11:48:35 +0800

    Delete start.sh

--- start.sh
+++
@@ -1,5 +0,0 @@
-php artisan serve --host=0.0.0.0 --port=$PORT &
-php artisan reverb:start --host=0.0.0.0 --port=8080 &
-php artisan queue:work --sleep=3 --tries=3 &
-php artisan schedule:work &
-wait
